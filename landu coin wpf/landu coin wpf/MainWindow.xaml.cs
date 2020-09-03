70a32cc22ee41de35a6748fd08c87788ef9fae32
Michael Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2020-09-03 21:13:16 +0300

    Add files via upload

---
+++ landu coin wpf/landu coin wpf/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace landu_coin_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread th;
+        public MainWindow()
+        {
+            InitializeComponent();
+            SplashScreen splashScreen = new SplashScreen("Image1.png");
+            splashScreen.Show(true);
+        }
+
+        private void opens(object obj)
+        {
+           //Application.Run(new (Window1));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBox.Text == "elkin" || TextBox1.Text == "elkin") {
+
+                Window1 taskWindow = new Window1();
+                taskWindow.Show();
+
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        { }
+           
+    }
+}
---
+++ landu coin wpf/landu coin wpf/Window1.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Diagnostics;
+
+namespace landu_coin_wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //создаем новый процесс
+            //Process proc = new Process();
+            //Запускаем Блокнто
+            //proc.StartInfo.FileName = @"Notepad.exe";
+            //proc.StartInfo.Arguments = "";
+            //proc.Start();
+
+            ProcessStartInfo stInfo =
+            new ProcessStartInfo(@"C:\Users\USER\source\repos\LanduS\Debug\LanduS.exe");
+
+            stInfo.UseShellExecute = false;
+            stInfo.CreateNoWindow = true;
+
+            //создаем новый процесс
+            Process proc = new Process();
+            proc.StartInfo = stInfo;
+            //Запускаем процесс
+            proc.Start();
+
+            //Ждем, пока блокнот запущен
+            proc.WaitForExit();
+
+           
+        }
+    }
+}
